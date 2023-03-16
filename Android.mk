a4f552ceaa805a38751a7651e557771f671d4137
Jai Shiv <quic_jshiv@quicinc.com>
2023-03-16 16:51:19 +0530

    iot-media: enable compilation for below target
    -uac/uvc required for qcs6490 and kona.
    
    Change-Id: Iaca9dd354203db8c99c2a552b7756edc048c34ee

--- Android.mk
+++ Android.mk
@@ -1,13 +1,17 @@
+ifeq ($(call is-board-platform-in-list,kona lahaina),true)
+
 IOT_MEDIA_PATH:= $(call my-dir)
 
-# Enable AI Director test & SNPE Lib
-# AI_DIRECTOR := true
-
-ifeq ($(TARGET_BOARD_PLATFORM),kona)
 include $(IOT_MEDIA_PATH)/umd-daemon/camera/Android.mk
 include $(IOT_MEDIA_PATH)/umd-daemon/daemon/Android.mk
-ifeq ($(AI_DIRECTOR), true)
+
+ifeq ($(TARGET_BOARD_PLATFORM),kona)
+# Enable AI Director test
+# AI_DIRECTOR := true
+ifeq ($(AI_DIRECTOR),true)
 include $(IOT_MEDIA_PATH)/ai-director-test/Android.mk
 endif
 include $(IOT_MEDIA_PATH)/snpe_wrapper/Android.mk
+endif
+
 endif
--- iot-media-system.mk
+++ iot-media-system.mk
@@ -2,16 +2,15 @@
 # and pass it on to UMD-Gadget module
 PRODUCT_PACKAGES += umd-daemon
 
+ifeq ($(TARGET_BOARD_PLATFORM),kona)
 # AI Director test & SNPE Lib
 # AI_DIRECTOR := true
 ifeq ($(AI_DIRECTOR), true)
 PRODUCT_PACKAGES += ai_director_test
+PRODUCT_PACKAGES += libSNPE
 endif
 
 # SNPE wrapper library
 PRODUCT_PACKAGES += libai_snpe_wrapper
-
-# SNPE libraries
-ifeq ($(AI_DIRECTOR),true)
-PRODUCT_PACKAGES += libSNPE
 endif
+
