de08f322d95b828a21cee8378fcf18c03ee21234
Deepali Reddy Karra <quic_dkarra@quicinc.com>
2023-06-08 10:34:52 +0100

    umd-adaptor: Enable compilation for qcm6490
    Change-Id: I363b93ac8ea0b7c2e0f3bee4d2758f11d7ba6574

--- iot-media-system.mk
+++ iot-media-system.mk
@@ -1,9 +1,10 @@
 # umd-daemon application to access camera and audio frames
 # and pass it on to UMD-Gadget module
 
-ifeq ($(TARGET_BOARD_PLATFORM),kona)
 PRODUCT_PACKAGES += libumd-adaptor
 PRODUCT_PACKAGES += vendor.qti.hardware.umd@1.0-service
+
+ifeq ($(TARGET_BOARD_PLATFORM),kona)
 # AI Director test & SNPE Lib
 # AI_DIRECTOR := true
 ifeq ($(AI_DIRECTOR), true)
