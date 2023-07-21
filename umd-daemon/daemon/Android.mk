6e167f858f26cf670472025e7bce17dba5d3d197
Deepali Reddy Karra <quic_dkarra@quicinc.com>
2023-07-21 19:18:39 +0530

    umd-adaptor: Add H264 support using codec2
    Change-Id: I8dfedd43a854f0e3af16fb7377dacc13359c18df

--- umd-daemon/daemon/Android.mk
+++ umd-daemon/daemon/Android.mk
@@ -4,6 +4,7 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE = libumd-adaptor
 LOCAL_PROPRIETARY_MODULE := true
+LOCAL_CFLAGS += -fcxx-exceptions
 
 ifeq ($(TARGET_BOARD_PLATFORM),kona)
 LOCAL_CFLAGS += -DTARGET_KONA
@@ -44,11 +45,31 @@ LOCAL_SHARED_LIBRARIES := \
     android.hardware.graphics.allocator@3.0 \
     android.hardware.graphics.mapper@3.0 \
     android.hardware.graphics.common@1.0 \
+    android.hardware.graphics.bufferqueue@2.0 \
     vendor.qti.hardware.umd@1.0
 
 LOCAL_STATIC_LIBRARIES := \
     android.hardware.camera.common@1.0-helper
 
 LOCAL_HEADER_LIBRARIES := libqtiumdheaders
+
+
+ifneq ($(TARGET_KERNEL_VERSION), 4.19)
+LOCAL_CFLAGS += -DENABLE_H264
+
+LOCAL_SHARED_LIBRARIES += \
+    libqcodec2_core \
+    libcodec2_vndk \
+    libcodec2 \
+    libqtic2module
+
+LOCAL_HEADER_LIBRARIES += libqcodec2_core_headers \
+                          libqcodec2_base_headers \
+                          libqcodec2_basecodec_headers \
+                          libcodec2_internal \
+                          display_intf_headers \
+                          libqcodec2_core_api_headers \
+                          libqtic2headers
+endif
 
 include $(BUILD_SHARED_LIBRARY)
